4b8a20831085380ef7d01fbef9e1a6618eda161c
hyezzl <xia2ue@gmail.com>
2025-09-05 16:24:34 +0900

    미니선택지 로직
    되는지는 오브젝트랑 상호작용 해보아야 함.

--- Assets/02Script/UI/MiniSelection.cs
+++ Assets/02Script/UI/MiniSelection.cs
@@ -9,11 +9,19 @@ public class MiniSelection : MonoBehaviour
     [SerializeField] private CanvasGroup group; // 선택지 전체 그룹
     [SerializeField] private Button yesBTN;
     [SerializeField] private Button noBTN;
+    [SerializeField] private Image yesFocus;
+    [SerializeField] private Image noFocus;
 
     private bool standbyInput = false; // 사용자 입력
 
     // 선택값
     private int focusIdx = -1;
+    private List<Button> buttons;
+
+    private void Awake()
+    {
+        buttons = new List<Button> { yesBTN, noBTN };
+    }
 
     private void OnEnable()
     {
@@ -31,11 +39,75 @@ public class MiniSelection : MonoBehaviour
     private void OnMiniSelection(UIEvents.OccurYN evt) { 
         standbyInput = true;
         group.alpha = 1f;
+        group.blocksRaycasts = true;
+        group.interactable = true;
+
+        yesFocus.enabled = false;
+        noFocus.enabled = false;
+
+        SetFocus(0);
+    }
+
+
+    private void Update()
+    {
+        if (!standbyInput) return;
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            int nextIdx = focusIdx + 1;
+            SetFocus(nextIdx);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            int preIdx = focusIdx - 1;
+            SetFocus(preIdx);
+        }
+        else if (Input.GetKeyDown(KeyCode.Space)) {
+            if (focusIdx == 0) OnChoice(true);
+            else if(focusIdx == 1) OnChoice(false);
+        }
+    }
+
+    private void SetFocus(int idx) {
+        if (idx == focusIdx) return;
+        idx = Mathf.Clamp(idx, 0, 1);
+
+        // 이전 포커스 끄기
+        if (focusIdx >= 0 && focusIdx < 2) {
+            Button preFocus = buttons[focusIdx];
+            Image preFocusImg = focusIdx == 0 ? yesFocus : noFocus;
+            preFocusImg.enabled = false;
+        }
+
+        focusIdx = idx;
+
+        // 현재 포커스 활성화
+        Button focus = buttons[focusIdx];
+        Image focusImg = focusIdx == 0 ? yesFocus : noFocus;
+        focusImg.enabled = true;
+
+        // 선택된 버튼에 포커스 설정
+        buttons[focusIdx].GetComponent<UnityEngine.UI.Button>().Select();
     }
 
     private void OnChoice(bool YN) {
         if (!standbyInput) return;
 
-        //EventBus.Instance.Publish<UIEvents.SelectYN>(new UIEvents.SelectYN(true));
+        if (YN)
+        {
+            // todo : 추가 처리
+            EventBus.Instance.Publish<UIEvents.SelectYN>(new UIEvents.SelectYN(true));
+        }
+        else {
+            // todo : 추가 처리
+            EventBus.Instance.Publish<UIEvents.SelectYN>(new UIEvents.SelectYN(false));
+        }
+
+        // 창 비활성화
+        group.alpha = 0f;
+        group.blocksRaycasts = false;
+        group.interactable = false;
+        standbyInput = false;
     }
 }
