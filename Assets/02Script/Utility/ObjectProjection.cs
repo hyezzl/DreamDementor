d867ac608985e0fd37d5e1300aba863a5613006e
jongwon <ghlrvy123@gmail.com>
2025-09-18 23:04:26 +0900

    HappyStage Add Object
    풀 추가, 구역 나누기.

--- Assets/02Script/Utility/ObjectProjection.cs
+++ Assets/02Script/Utility/ObjectProjection.cs
@@ -1,14 +1,18 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+/// <summary>
+/// 3D 오브젝트용
+/// </summary>
 
 public class ObjectProjection : MonoBehaviour
 {
     [SerializeField] private Transform player;
-    [SerializeField] private float alphaRatio = 0.8f;
+    [SerializeField] private float alphaRatio = 0.6f;
+
+    private List<Renderer> renderers = new List<Renderer>();
 
     private PlayerController pc;
-    private List<SpriteRenderer> renderers = new List<SpriteRenderer>();
 
     private bool isThree = false;
 
@@ -17,7 +21,6 @@ public class ObjectProjection : MonoBehaviour
         pc = FindAnyObjectByType<PlayerController>();
         if (pc == null) Debug.Log("ObjectProjection - Failed to Load PlayerController");
     }
-
     private void OnEnable()
     {
         EventBus.Instance.Subscribe<GameEvents.AspectChange>(ChangeAspect);
@@ -25,9 +28,9 @@ public class ObjectProjection : MonoBehaviour
     private void OnDisable()
     {
         EventBus.Instance.Unsubscribe<GameEvents.AspectChange>(ChangeAspect);
-
     }
-    private void ChangeAspect(GameEvents.AspectChange evt) {
+    private void ChangeAspect(GameEvents.AspectChange evt)
+    {
         // 3인칭일 경우에만 Projection
         switch (evt.mode)
         {
@@ -46,28 +49,31 @@ public class ObjectProjection : MonoBehaviour
         Transparency();
     }
 
-    private void Transparency() {
-        foreach (SpriteRenderer sr in renderers) {
-            Color color = sr.color;
-            color.a = 1f; // 되돌림
-            sr.color = color;
+    private void Transparency()
+    {
+        foreach (Renderer rend in renderers)
+        {
+            Color color = rend.material.color;
+            color.a = 1f;
+            rend.material.color = color;
         }
         renderers.Clear();
 
-        // 카메라에서 캐릭터로 레이캐스트
         Vector3 dir = player.position - transform.position;
         RaycastHit[] hits = Physics.RaycastAll(transform.position, dir.normalized);
 
         foreach (RaycastHit hit in hits)
         {
-            SpriteRenderer sr = hit.collider.GetComponent<SpriteRenderer>();
-            if (sr != null && hit.collider.gameObject != player.gameObject) 
+            Renderer rend = hit.collider.GetComponent<Renderer>();
+            if (rend != null & hit.collider.gameObject != player.gameObject)
             {
-                Color color = sr.color;
+                Color color = rend.material.color;
+
                 color.a = alphaRatio;
-                sr.color = color;
-                renderers.Add(sr);
+                rend.material.color = color;
+                renderers.Add(rend);
             }
         }
     }
+
 }
