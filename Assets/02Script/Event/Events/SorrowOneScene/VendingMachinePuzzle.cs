24b1a8f70fef13faa432168ff8ce66d43ba6808e
hyezzl <xia2ue@gmail.com>
2026-01-29 22:07:59 +0900

    자판기 기능 완성
    메모UI뜨는건 나중에!

--- Assets/02Script/Event/Events/SorrowOneScene/VendingMachinePuzzle.cs
+++ Assets/02Script/Event/Events/SorrowOneScene/VendingMachinePuzzle.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VendingMachinePuzzle : MonoBehaviour
 {
@@ -10,19 +12,38 @@ public class VendingMachinePuzzle : MonoBehaviour
     public GameObject uncompleteVendingMachine;
     public GameObject completeVendingMachine;
 
+    // 두개의 버튼
+    public Button selectBtn;
+    public Button getBtn;
+
+    // 메모 애니메이터
+    public Animator memoAnim;
+
     private void Awake()
     {
         pm = transform.parent.GetComponent<PuzzleManager>();
         if (pm == null) Debug.Log("VendingMachinePuzzle - failed to Load PuzzleManager");
     }
 
+    private void Start()
+    {
+        selectBtn.gameObject.SetActive(false);
+        getBtn.gameObject.SetActive(false);
+    }
+
     private void OnEnable()
     {
         EventBus.Instance.Subscribe<PuzzleEvents.SO_WorkVendingMachine>(CompleteVendingMachine);
+
+        selectBtn.onClick.AddListener(OnClickMemoButton);
+        getBtn.onClick.AddListener(OnGetMemo);
     }
     private void OnDisable()
     {
         EventBus.Instance.Unsubscribe<PuzzleEvents.SO_WorkVendingMachine>(CompleteVendingMachine);
+
+        selectBtn.onClick.RemoveListener(OnClickMemoButton);
+        getBtn.onClick.RemoveListener(OnGetMemo);
     }
 
 
@@ -34,12 +55,45 @@ public class VendingMachinePuzzle : MonoBehaviour
         completeVendingMachine.SetActive(true);
         uncompleteVendingMachine.SetActive(false);
 
-        //효과음
+        // 효과음
+
+        // 메모 버튼 활성화
+        selectBtn.gameObject.SetActive(true);
+
 
         // Close
         //pm.CloseAllPuzzleUI();
-
         //EventBus.Instance.Publish<PuzzleEvents.CutOff>(new PuzzleEvents.CutOff(60001003));
+    }
+
+    // 메모 버튼 눌렸을 때
+    private void OnClickMemoButton() 
+    {
+        // 애니메이션 발생
+        if (memoAnim != null) { 
+            memoAnim.SetTrigger("FallDown");
+
+            StartCoroutine(MemoAnimation());
+        }
+    }
+
+    // 메모 습득 버튼 눌렸을 때
+    private void OnGetMemo() {
+        // 기본 UI내려감
+
+        // 메모 UI 켜짐
+        Debug.Log("메모UI켜짐");
 
     }
+
+    IEnumerator MemoAnimation() { 
+        yield return null;
+
+        yield return new WaitForSeconds(2f);
+
+        // 메모 습득 버튼 활성화
+        getBtn.gameObject.SetActive(true);
+        selectBtn.gameObject.SetActive(false);
+    }
 }
+
