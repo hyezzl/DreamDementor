43546051def9b73a9fec454323fb132b2740e3be
hyezzl <xia2ue@gmail.com>
2026-03-28 19:11:25 +0900

    슬픔맵 문UI

--- Assets/02Script/Player/FlashLight.cs
+++ Assets/02Script/Player/FlashLight.cs
@@ -10,12 +10,13 @@ public class FlashLight : MonoBehaviour
     [Header("Light Setting")]
     [SerializeField] private float rotationSpeed = 10f;
     [SerializeField] private GameObject lightObj;
+    [SerializeField] private float tempX = 25.32f;
 
     [Header("Gauge Setting")]
     [SerializeField] private Image gaugeImg;
     [SerializeField] private float maxTime = 20f;
     [SerializeField] private float curGauge;        // 나중에 serial XX
-    [SerializeField] private float recoverRatio = 1.5f; // 소모 대비 충전 비
+    [SerializeField] private float recoverRatio = 0.8f; // 소모 대비 충전 비
     [SerializeField] private float uistayTime = 3f;
 
     private PlayerMove pm;
@@ -97,7 +98,7 @@ public class FlashLight : MonoBehaviour
 
         float rotationY = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
 
-        Quaternion rotate = Quaternion.Euler(46.42f, rotationY, 0);
+        Quaternion rotate = Quaternion.Euler(tempX, rotationY, 0);
 
         // 부드러운 회전
         lightObj.transform.rotation = Quaternion.Slerp(lightObj.transform.rotation, rotate, Time.deltaTime * rotationSpeed);
